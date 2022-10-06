2591a4bfc5f82ea2c6a0bd8eac2a84b87d2a482f
benlloyd50 <benji6090@gmail.com>
2022-10-05 23:46:50 -0400

    Basically finished everything for an early proof of concept

--- readme.md
+++ readme.md
@@ -1,13 +1,14 @@
-# TODO App
+# TODO TUI
 Simple rust TUI application that keeps track of goals and tasks that you create and manage.
 
 ## Features
 - [ ] Create tasks that are recreatable
-- [ ] Check off tasks
-- [ ] View history of tasks
-- [ ] Add descriptions to tasks
-- [ ] Clean TUI to let sit in your terminal
-- [ ] One screen that shows all you need 
+- [X] Check off tasks
+- [X] View history of tasks
+- [X] Add descriptions to tasks
+- [X] Clean TUI to let sit in your terminal
+- [ ] Experiment with a <action\> <object\> command pattern
+- [ ] Kanban Addition
 
 ## Task Information
 - name
@@ -15,4 +16,6 @@ Simple rust TUI application that keeps track of goals and tasks that you create 
 - desc  *optional
 - due_date  *optional
 
-
+## Kanban Addition
+- another view of tasks that breaks them into seperate columns which function as categories
+- presents many challenges concerning how the data will be preseneted horizontally if it needs to be squished?
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::{
     io::{self, BufReader, Read},
 };
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 const TASKS_FILE_PATH: &str = "data/tasks.json";
 
-#[derive(Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Task {
     name: String,
     is_completed: bool,
@@ -15,11 +15,22 @@ struct Task {
     due_date: String,
 }
 
+impl Default for Task {
+    fn default() -> Self {
+        Task {
+            name: "?".to_string(),
+            is_completed: false,
+            desc: "none".to_string(),
+            due_date: "none".to_string(),
+        }
+    }
+}
+
 impl fmt::Display for Task {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "| {: ^23} => {: <72} Due: {: ^10} |",
+            "│ {: ^26} => {: <69} Due: {: ^10} │",
             self.name, self.desc, self.due_date
         )
     }
@@ -46,38 +57,109 @@ fn main() {
     let mut all_tasks: Vec<Task> =
         serde_json::from_str(&task_string).expect("Json could not be deserialized");
 
-    // Display any existing, non complete tasks
-    print_tasks(
-        all_tasks
-            .iter()
-            .filter(|t| !t.is_completed)
-            .cloned() //prevents a copy of the vec from being discarded and recreated with collect
-            .collect(),
-    );
-
     let mut input_string = String::new();
+    let mut should_print: bool = true;
     loop {
+        //update active tasks and display to user
+        if should_print {
+            let active_tasks: Vec<Task> = all_tasks
+                .iter()
+                .filter(|t| !t.is_completed)
+                .cloned() //prevents a copy of the vec from being discarded and recreated with collect
+                .collect();
+            print_tasks(&active_tasks, "Ongoing Tasks");
+        } else {
+            should_print = true;
+        }
+
         input_string.clear();
         io::stdin()
             .read_line(&mut input_string)
             .expect("Input was invalid");
         if input_string.trim().eq_ignore_ascii_case("finish") {
             finish_task(&mut all_tasks);
+        } else if input_string.trim().eq_ignore_ascii_case("create") {
+            create_task(&mut all_tasks);
+        } else if input_string.trim().eq_ignore_ascii_case("delete") {
+            delete_task(&mut all_tasks);
+        } else if input_string.trim().eq_ignore_ascii_case("history") {
+            print_tasks(&all_tasks, "Every Task");
+            should_print = false;
         } else if input_string.trim().eq_ignore_ascii_case("quit") {
+            save_all_tasks(&all_tasks);
             break;
-            //Save new tasks to file
         }
     }
 }
 
-fn print_tasks(active_tasks: Vec<Task>) {
-    println!("┌{:-^117}┐", "Ongoing Tasks");
-    for task in active_tasks.iter() {
+fn print_tasks(tasks: &Vec<Task>, header: &str) {
+    println!("┌{:─^117}┐", header);
+    for task in tasks.iter() {
         println!("{}", task)
     }
-    println!("└{:-<117}┘", "What's next?");
+    println!("└{:─<117}┘", "What's next?");
 }
 
 fn finish_task(all_tasks: &mut Vec<Task>) {
-    println!("What task did you finish?");
+    println!("What task did you finish? (index)");
+    let mut input_string = String::new();
+    io::stdin()
+        .read_line(&mut input_string)
+        .expect("Input was invalid");
+    let idx = match input_string.trim().parse::<i32>() {
+        Ok(idx) => idx,
+        Err(err) => {
+            panic!("Can not do that thing sorry im out, {}", err);
+        }
+    };
+    // all_tasks.remove(idx as usize);
+    all_tasks[idx as usize].is_completed = true;
+}
+
+fn delete_task(all_tasks: &mut Vec<Task>) {
+    println!("What task did you want to delete? (index)");
+    let mut input_string = String::new();
+    io::stdin()
+        .read_line(&mut input_string)
+        .expect("Input was invalid");
+    let idx = match input_string.trim().parse::<i32>() {
+        Ok(idx) => idx,
+        Err(err) => {
+            panic!("Can not do that thing sorry im out, {}", err);
+        }
+    };
+    all_tasks.remove(idx as usize);
+}
+
+fn create_task(all_tasks: &mut Vec<Task>) {
+    let fields = vec!["name", "description", "due date"];
+    let mut field = String::new();
+    let mut new_task: Task = Default::default();
+    for i in 0..3 {
+        field.clear();
+        println!("Enter a {} for the new task", fields[i]);
+        io::stdin()
+            .read_line(&mut field)
+            .expect("Input was invalid");
+        field = field.trim().to_string();
+        if i == 0 {
+            new_task.name = field.clone();
+        } else if i == 1 {
+            new_task.desc = field.clone();
+        } else if i == 2 {
+            new_task.due_date = field.clone();
+        }
+    }
+    all_tasks.push(new_task);
+}
+
+fn save_all_tasks(all_tasks: &Vec<Task>) {
+    let json_string = match serde_json::to_string_pretty(&all_tasks) {
+        Ok(val) => val,
+        Err(err) => {
+            panic!("Could not convert tasks into json {}", err);
+        }
+    };
+    fs::write(TASKS_FILE_PATH, json_string)
+        .expect(&format!("Could not write to the file {}", TASKS_FILE_PATH));
 }
